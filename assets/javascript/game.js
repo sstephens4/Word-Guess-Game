6a22d8d98a19c002a6a16ed79a8809ecfa095098
Serena <serenastephens@Serenas-MacBook-Air.local>
2019-02-15 16:53:47 -0700

    Edited index layout and added more styling to page

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Create an array of Disney Characters
+var disneyCharacters = ['Daffy Duck', 'Minnie Mouse', 'Daisy Duck', 'Goofy',
+    'Tinkerbell', 'Peter Pan', 'Pluto', 'Mickey Mouse', 'Ariel', 'Belle'];
+
+// Choose character randomly
+var randNum = Math.floor[Math.random() * disneyCharacters.length]);
+// Create underscores based on length of chosen character.
+var chosenWord = disneyCharacters[randNum];
+console.log("Hello World");
+// Get users guess
+// Check if guess is right
+// if right push to right array
+// if wrong push to wrong array
